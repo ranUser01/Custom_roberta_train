ec1a101910fea53fc339390af4e70415e2abc404
janusz <jawi@itu.dk>
2023-06-16 21:03:05 +0200

    faulthandler

--- run_language_modeling.py
+++ run_language_modeling.py
@@ -18,7 +18,8 @@ Fine-tuning the library models for language modeling on a text file (GPT, GPT-2,
 GPT and GPT-2 are fine-tuned using a causal language modeling (CLM) loss while BERT and RoBERTa are fine-tuned
 using a masked language modeling (MLM) loss.
 """
-
+import faulthandler
+faulthandler.enable()
 import torch
 import logging
 import math
@@ -43,7 +44,6 @@ from transformers import (
 
 
 logger = logging.getLogger(__name__)
-
 
 MODEL_CONFIG_CLASSES = list(MODEL_WITH_LM_HEAD_MAPPING.keys())
 MODEL_TYPES = tuple(conf.model_type for conf in MODEL_CONFIG_CLASSES)
